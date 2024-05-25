a05a87e4151835699704f32e9801d1265958ab5f
David Tanzer <david@davidtanzer.net>
2024-05-25 12:36:40 +0200

    Lab 05: Game logic from Service class

---
+++ src/Game.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react"
+import { BeatThePooGame } from "./BeatThePooGame"
+import { VisualStatus } from "./VisualStatus"
+import { GuessesHistory } from "./GuessesHistory"
+
+export interface GameProps {
+	gameApi: BeatThePooGame,
+}
+export function Game({ gameApi }: GameProps) {
+	const [startTime, setStartTime] = useState(Date.now())
+	const [timeLeft, setTimeLeft] = useState(15)
+	const [wrongGuesses, setWrongGuesses] = useState(0)
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			const secondsElapsed = Math.floor((Date.now() - startTime) / 1000)
+			const secondsLeft = 15-secondsElapsed
+
+			setTimeLeft(secondsLeft)
+			if(secondsLeft <= 0) {
+				setWrongGuesses(gameApi.loseImmediately().wrongGuesses)
+				clearInterval(interval)
+			}
+		}, 200)
+		return () => {
+			clearInterval(interval)
+		}
+	}, [startTime, gameApi])
+
+	return <>
+		<div>{timeLeft}</div>
+		<div><VisualStatus failedGuesses={wrongGuesses}/></div>
+		<GuessesHistory previousGuesses='Previous Guesses'/>
+	</>
+}
--- src/PlayerName.tsx
+++ src/PlayerName.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react"
+import { BeatThePooGame } from "./BeatThePooGame"
 
-export function PlayerName() {
+export interface PlayerNameProps {
+	gameApi: BeatThePooGame,
+}
+export function PlayerName({ gameApi }: PlayerNameProps) {
 	const [name, setName] = useState('')
 	const [nameEntered, setNameEntered] = useState(false)
 
@@ -10,7 +14,12 @@ export function PlayerName() {
 				<span data-testid="player-name">{name}</span>:
 				<input type="text" value={name} placeholder="Jane Doe" 
 					onChange={e => setName(e.target.value)} data-testid="player-name"
-					onKeyDown={e => setNameEntered(e.key === 'Enter') }/>
+					onKeyDown={e => {
+						if(e.key === 'Enter') {
+							gameApi.playerName = (e.target as HTMLInputElement).value
+							setNameEntered(true)
+						}
+					}}/>
 			}
 		</label>
 	</div>
