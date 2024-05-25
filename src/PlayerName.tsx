f0e8dcf646064f7ecc4fea06bac7542b30c51cca
David Tanzer <david@davidtanzer.net>
2024-05-25 07:09:48 +0200

    Lab 03: Added tests for PlayerName

--- src/PlayerName.tsx
+++ src/PlayerName.tsx
@@ -2,11 +2,16 @@ import { useState } from "react"
 
 export function PlayerName() {
 	const [name, setName] = useState('')
+	const [nameEntered, setNameEntered] = useState(false)
 
 	return <div>
 		<label>
-			Player Name: <input type="text" value={name} placeholder="Jane Doe" 
-				onChange={e => setName(e.target.value)} />
+			Player Name: { nameEntered?
+				<span data-testid="player-name">{name}</span>:
+				<input type="text" value={name} placeholder="Jane Doe" 
+					onChange={e => setName(e.target.value)} data-testid="player-name"
+					onKeyDown={e => setNameEntered(e.key === 'Enter') }/>
+			}
 		</label>
 	</div>
 }
--- src/__tests__/PlayerName.spec.tsx
+++ src/__tests__/PlayerName.spec.tsx
@@ -1,7 +1,33 @@
 import '@testing-library/jest-dom'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { PlayerName } from '../PlayerName'
 
 describe('<PlayerName />', () => {
-	it('does something', async () => {
-		expect(true).toEqual(false)
+	it('contains empty text field before first input', async () => {
+		render(<PlayerName />)
+		
+		await screen.findByPlaceholderText('Jane Doe')
+
+		expect(screen.getByPlaceholderText('Jane Doe')).toHaveAttribute('value', '')
+	})
+
+	it('updates the text field on input', async () => {
+		const {container} = render(<PlayerName />)
+
+		await fireEvent.change(container.getElementsByTagName('input').item(0)!, {target: { value: 'hello'}})
+		await screen.findByPlaceholderText('Jane Doe')
+
+		expect(screen.getByPlaceholderText('Jane Doe')).toHaveAttribute('value', 'hello')
+	})
+
+	it('changes the text field to span on [Enter]', async () => {
+		const {container} = render(<PlayerName />)
+
+		await fireEvent.change(container.getElementsByTagName('input').item(0)!, {target: { value: 'hello'}})
+		await fireEvent.keyDown(container.getElementsByTagName('input').item(0)!, { key: 'Enter' })
+		await screen.findByTestId('player-name')
+
+		expect(screen.getByTestId('player-name')).toBeInstanceOf(HTMLSpanElement)
+		expect(screen.getByTestId('player-name')).toHaveTextContent('hello')
 	})
 })
