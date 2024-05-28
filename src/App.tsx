c5345be9dd837da5cbced5770d1177c1ef18dce1
David Tanzer <david@davidtanzer.net>
2024-05-28 09:12:48 +0200

    Lab 08: Focus game & start timer

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Heading } from './Heading'
 import { PlayerName } from './PlayerName'
 import { BeatThePooGame } from './BeatThePooGame'
 import { Game } from './Game'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 export interface AppProps {
 	gameApi?: BeatThePooGame,
@@ -15,7 +15,7 @@ function App({ gameApi = new BeatThePooGame('tutorial') }: AppProps) {
 		<>
 			<Heading text="Beat the Poo"><span className='small'>A word guessing game</span></Heading>
 			<PlayerName gameApi={gameApi} onNameEntered={ () => setGameStarted(true) }/>
-			{ gameStarted? <Game gameApi={gameApi}/> : undefined }
+			<Game gameApi={gameApi} gameStarted={gameStarted} />
 		</>
 	)
 }
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { BeatThePooGame } from "./BeatThePooGame"
 import { VisualStatus } from "./VisualStatus"
 import { GuessesHistory } from "./GuessesHistory"
@@ -6,32 +6,45 @@ import { Hint } from "./Hint"
 
 export interface GameProps {
 	gameApi: BeatThePooGame,
+	gameStarted: boolean,
 }
-export function Game({ gameApi }: GameProps) {
+export function Game({ gameApi, gameStarted }: GameProps) {
 	const [startTime, setStartTime] = useState(Date.now())
 	const [timeLeft, setTimeLeft] = useState(15)
 	const [gameState, setGameState] = useState(gameApi.gameState)
 
+	const gameElementRef = useRef<HTMLDivElement | null>(null)
+
 	useEffect(() => {
-		const interval = setInterval(() => {
-			const secondsElapsed = Math.floor((Date.now() - startTime) / 1000)
-			const secondsLeft = 15-secondsElapsed
-
-			setTimeLeft(secondsLeft)
-			if(secondsLeft <= 0) {
-				clearInterval(interval)
-				setGameState(gameApi.loseImmediately())
-			}
-		}, 200)
-		return () => {
-			clearInterval(interval)
+		let interval: NodeJS.Timeout | undefined
+		if(gameStarted) {
+			setStartTime(Date.now())
+			interval = setInterval(() => {
+				const secondsElapsed = Math.floor((Date.now() - startTime) / 1000)
+				const secondsLeft = 15-secondsElapsed
+	
+				setTimeLeft(secondsLeft)
+				if(secondsLeft <= 0) {
+					if(interval) { clearInterval(interval) }
+					setGameState(gameApi.loseImmediately())
+				}
+			}, 200)
 		}
-	}, [startTime, gameApi])
+		return () => {
+			if(interval) { clearInterval(interval) }
+		}
+	}, [startTime, gameApi, gameStarted])
 
+	useEffect(() => {
+		if(gameStarted) {
+			gameElementRef.current?.focus()
+		}
+	}, [gameElementRef, gameStarted])
+	
 	return <div tabIndex={1} onKeyDown={e => { if(e.key >= 'a' && e.key <= 'z') {
 		setStartTime(Date.now())
 		setGameState(gameApi.guess(e.key))
-	}}}>
+	}}} ref={gameElementRef}>
 		<div>{timeLeft}</div>
 		<Hint hint={gameState.hint} />
 		<div><VisualStatus failedGuesses={gameState.wrongGuesses}/></div>
